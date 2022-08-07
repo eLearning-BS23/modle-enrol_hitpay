4e7d965925eff8f8a35c6da93e30843a47fa08c5
Umme Kawser Sinthia <sinthy08@gmail.com>
2022-08-07 20:56:37 +0600

    added documentation and updated settings

--- settings.php
+++ settings.php
@@ -18,7 +18,7 @@
  * hitpay enrolments plugin settings and presets.
  *
  * @package    enrol_hitpay
- * @copyright  2021 Brain station 23 ltd.
+ * @copyright  2022 Brain station 23 ltd.
  * @author     Brain station 23 ltd.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -31,69 +31,23 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_heading('enrol_hitpay_settings', '',
         get_string('pluginname_desc', 'enrol_hitpay')));
 
-    $settings->add(new admin_setting_configtext('enrol_hitpay/apiurl',
-        get_string('apiurl', 'enrol_hitpay'),
-        get_string('apiurl_desc', 'enrol_hitpay'), '', PARAM_TEXT));
-
     $settings->add(new admin_setting_configtext('enrol_hitpay/apikey',
         get_string('apikey', 'enrol_hitpay'),
         get_string('apikey_desc', 'enrol_hitpay'), '', PARAM_TEXT));
 
-    $settings->add(new admin_setting_configtext('enrol_hitpay/productionenv',
-        get_string('productionenv', 'enrol_hitpay'),
-        get_string('productionenv_desc', 'enrol_hitpay'), 'False', PARAM_TEXT));
+    // Production Environment.
+    $yesno = array(
+        new lang_string('no'),
+        new lang_string('yes'),
+    );
+    $settings->add(new admin_setting_configselect('auth_cas/proxycas',
+        new lang_string('productionenv', 'enrol_hitpay'),
+        new lang_string('productionenv_desc', 'enrol_hitpay'), 0 , $yesno));
 
-
-//    $settings->add(new admin_setting_configcheckbox('enrol_hitpay/mailstudents',
-//        get_string('mailstudents', 'enrol_hitpay'), '', 0));
-//
-//    $settings->add(new admin_setting_configcheckbox('enrol_hitpay/mailteachers',
-//        get_string('mailteachers', 'enrol_hitpay'), '', 0));
-//
-//    $settings->add(new admin_setting_configcheckbox('enrol_hitpay/mailadmins',
-//        get_string('mailadmins', 'enrol_hitpay'), '', 0));
-//
-//    // Note: let's reuse the ext sync constants and strings here, internally it is very similar,
-//    // it describes what should happen when users are not supposed to be enrolled any more.
-//    $options = array(
-//        ENROL_EXT_REMOVED_KEEP => get_string('extremovedkeep', 'enrol'),
-//        ENROL_EXT_REMOVED_SUSPENDNOROLES => get_string('extremovedsuspendnoroles', 'enrol'),
-//        ENROL_EXT_REMOVED_UNENROL => get_string('extremovedunenrol', 'enrol'),
-//    );
-//    $settings->add(new admin_setting_configselect('enrol_hitpay/expiredaction',
-//        get_string('expiredaction', 'enrol_hitpay'),
-//        get_string('expiredaction_help', 'enrol_hitpay'), ENROL_EXT_REMOVED_SUSPENDNOROLES, $options));
-//
-//    // Enrol instance defaults.
-//    $settings->add(new admin_setting_heading('enrol_hitpay_defaults',
-//        get_string('enrolinstancedefaults', 'admin'), get_string('enrolinstancedefaults_desc', 'admin')));
-////
-//    $options = array(ENROL_INSTANCE_ENABLED => get_string('yes'),
-//        ENROL_INSTANCE_DISABLED => get_string('no'));
-////
-//    $settings->add(new admin_setting_configselect('enrol_hitpay/status',
-//        get_string('status', 'enrol_hitpay'), get_string('status_desc', 'enrol_hitpay'),
-//        ENROL_INSTANCE_ENABLED, $options));
-////
-//    $settings->add(new admin_setting_configtext('enrol_hitpay/cost',
-//        get_string('cost', 'enrol_hitpay'), '', 0, PARAM_FLOAT, 4));
-//
-//    $hitpaycurrencies = enrol_get_plugin('hitpay')->get_currencies();
-//    $settings->add(new admin_setting_configselect('enrol_hitpay/currency',
-//        get_string('currency', 'enrol_hitpay'), '', 'USD', $hitpaycurrencies));
-//
-//    if (!during_initial_install()) {
-//        $options = get_default_enrol_roles(context_system::instance());
-//        $student = get_archetype_roles('student');
-//        $student = reset($student);
-//        $settings->add(new admin_setting_configselect('enrol_hitpay/roleid',
-//            get_string('defaultrole', 'enrol_hitpay'),
-//            get_string('defaultrole_desc', 'enrol_hitpay'),
-//            $student->id ?? null,
-//            $options));
-//    }
-//
-//    $settings->add(new admin_setting_configduration('enrol_hitpay/enrolperiod',
-//        get_string('enrolperiod', 'enrol_hitpay'),
-//        get_string('enrolperiod_desc', 'enrol_hitpay'), 0));
+    if($yesno == 'no') {
+        $apiurl = "https://api.sandbox.hit-pay.com/v1/payment-requests/";
+    }
+    else {
+        $apiurl ="https://api.hit-pay.com/v1/payment-requests/";
+    }
 }
